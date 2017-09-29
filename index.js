dd415a22639f2b4e4b6593c9a453948e62d28d90
Chuhan Gao <chuhan@royal-12.cs.wisc.edu>
2017-09-28 19:40:23 -0500

    ping

--- index.js
+++ index.js
@@ -2,10 +2,23 @@ const express = require('express')
 const app = express()
 
 app.get('/',function (req,res){
-  res.send('Hello World')
+  res.send("Ping test completed!")
 
 })
 
 app.listen(3000,function(){
-  console.log('App is listening on port 3000')
+  var net = require('net');
+var hosts = [['google.com', 80], ['stackoverflow.com', 80], ['google.com', 444]];
+hosts.forEach(function(item) {
+    var sock = new net.Socket();
+    sock.setTimeout(2500);
+    sock.on('connect', function() {
+        console.log(item[0]+':'+item[1]+' is up.');
+        sock.destroy();
+    }).on('error', function(e) {
+        console.log(item[0]+':'+item[1]+' is down: ' + e.message);
+    }).on('timeout', function(e) {
+        console.log(item[0]+':'+item[1]+' is down: timeout');
+    }).connect(item[1], item[0]);
+});
 })
